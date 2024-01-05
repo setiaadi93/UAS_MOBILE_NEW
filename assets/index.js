0cc3219c42d5081b1001b5f9382f0cd096965769
setiaadi93 <153510929+setiaadi93@users.noreply.github.com>
2024-01-05 20:58:48 +0700

    Add files via upload

---
+++ assets/index.js
@@ -0,0 +1,28 @@
+import AppLogo from './AppLogo.png';
+import IconAdd from './IconAdd.png';
+import IconLove from './IconLove.png';
+import IconFacebook from './facebook-icon.png';
+import Kucing from './kucing.jpeg'
+import Verified from './verified.png'
+import Loading from './loading.gif'
+import Logo from './Logo1.png'
+import acun from './acun.png'
+import task from './task.png'
+import rprt from './rprt.png'
+import sec2 from './sec2.png'
+import list from './list.png'
+export {
+    AppLogo,
+    IconAdd,
+    IconLove,
+    IconFacebook,
+    Kucing,
+    Verified,
+    Loading,
+    Logo,
+    acun,
+    task,
+    rprt,
+    sec2,
+    list
+}
